d3edb7bfab06ece2e2fbf5cbbbffdb68c2226da3
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-06-13 14:27:52 +0200

    FIX path

--- httpdocs/api/abonnements/status.php
+++ httpdocs/api/abonnements/status.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once '../config/config.php';
+require_once '../../config/config.php';
 
 // Vérification de l'authentification
 session_start();
