474a8964c42b201b497a45a4aaf712e1716716df
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-06-16 09:49:14 +0200

    Revert "UPDATE translations"
    This reverts commit f159cca3c720f1481fcfb8af4c61ba03bb01166a.

---
+++ httpdocs/src/lang/fr.js
@@ -0,0 +1,15 @@
+export default {
+    subscription: {
+        title: 'Abonnement',
+        loading: 'Chargement des options d\'abonnement...',
+        init_error: 'Erreur lors de l\'initialisation des achats in-app',
+        purchase: 'S\'abonner',
+        active: 'Abonnement actif',
+        expires: 'Expire le {date}',
+        monthly: 'Abonnement mensuel',
+        quarterly: 'Abonnement trimestriel',
+        semiannual: 'Abonnement semestriel',
+        annual: 'Abonnement annuel',
+        mobile_only: 'Les achats in-app ne sont disponibles que sur les appareils mobiles'
+    }
+}; 
