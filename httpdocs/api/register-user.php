44983ce39e8db0dc61eb649916b24f0e6775ec7c
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-06-19 16:11:50 +0200

    UPDATE api allowed origins

--- httpdocs/api/register-user.php
+++ httpdocs/api/register-user.php
@@ -18,6 +18,12 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With');
 header('Access-Control-Allow-Credentials: true');
 
+// ... headers CORS déjà envoyés ici ...
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 try {
     $data = json_decode(file_get_contents('php://input'), true);
     $username = $data['username'] ?? '';
